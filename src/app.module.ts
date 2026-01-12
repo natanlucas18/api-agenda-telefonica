27d4d4be9435c0e4ebca0d750dd1574425f0e442
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-12 15:57:43 -0300

    adiciona configurações de variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import globalConfig from './global.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [globalConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [globalConfig.KEY],
+      useFactory: (globalConfigurations: ConfigType<typeof globalConfig>) => {
+        return {
+          type: globalConfigurations.database.type,
+          host: globalConfigurations.database.host,
+          port: globalConfigurations.database.port,
+          username: globalConfigurations.database.username,
+          database: globalConfigurations.database.database,
+          password: globalConfigurations.database.password,
+          autoLoadEntities: globalConfigurations.database.autoLoadEntities,
+          synchronize: globalConfigurations.database.synchronize,
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/global.config.ts
@@ -0,0 +1,14 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs('global', () => ({
+    database: {
+        type: process.env.DATABASE_TYPE as 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: String(process.env.DATABASE_USERNAME),
+        database: String(process.env.DATABASE_DATABASE),
+        password: String(process.env.DATABASE_PASSWORD),
+        autoLoadEntities: Boolean(process.env.AUTO_LOAD_ENTITIES),
+        synchronize: Boolean(process.env.SYNCHRONIZE)
+    }
+}))
