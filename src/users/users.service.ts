08f0f4746df319c5af135d4c95c37ed903df4732
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-14 19:26:27 -0300

    implementa criação de novos usuários, busca por id, atualização e remoção

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,22 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
+import { HashingService } from "src/auth/hashing/hashing.service";
+import { ResponseUserDto } from "./dtos/response-user.dto";
+import { TokenPayloadDto } from "src/auth/dto/token-payload.dto";
 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectRepository(User)
         private readonly userRepo: Repository<User>,
+        private readonly hashingService: HashingService,
     ) {}
 
-    async create(createUserDto: CreateUserDto) {
+    async create(createUserDto: CreateUserDto):Promise<ResponseUserDto> {
         const isExists = await this.userRepo.findOneBy({
             email: createUserDto.email,
         });
@@ -20,24 +24,67 @@ export class UsersService {
             throw new ConflictException('Esse e-mail já está cadastrado.');
         }
 
+        const passwordHash = await this.hashingService.hash(createUserDto.password);
         const user = this.userRepo.create({
             name: createUserDto.name,
             email: createUserDto.email,
-            password: createUserDto.password,
+            passwordHash,
         });
+        return await this.userRepo.save(user);
+    }
+
+    async findOne(id: string): Promise<ResponseUserDto> {
+        const user = await this.userRepo.findOneBy({
+            id,
+        });
+
+        if (!user) {
+            throw new NotFoundException('Usuário não encontrado');
+        }
+
+        return user;
+    }
+
+    async update(id: string, updateUserDto: UpdateUserDto, tokenPayloadDto:TokenPayloadDto):Promise<ResponseUserDto> {
+        const userData = {
+            name: updateUserDto?.name,
+        };
+
+        if(updateUserDto?.password) {
+            const passwordHash = await this.hashingService.hash(
+                updateUserDto.password,
+            );
+            userData['passwordHash'] = passwordHash;
+        };
+        const user = await this.userRepo.preload({
+            id,
+            ...userData,
+        });
+
+        if(!user) {
+            throw new NotFoundException('Usuário não encontrado');
+        }
+
+        if(user.id !== tokenPayloadDto.sub) {
+            throw new ForbiddenException('Você não pode atualizar esse usuário');
+        }
 
         return await this.userRepo.save(user);
     }
 
-    async findOne(id: string) {
-        console.log(id);
-    }
+    async remove(id: string, tokenPayloadDto:TokenPayloadDto):Promise<ResponseUserDto> {
+        const user = await this.userRepo.findOneBy({
+            id,
+        });
+        
+        if (!user) {
+            throw new NotFoundException('Usuário não encontrado');
+        };
 
-    async update(id: string, updateUserDto: UpdateUserDto) {
-        console.log(id, updateUserDto);
-    }
+        if (user.id !== tokenPayloadDto.sub) {
+            throw new ForbiddenException('Você não pode remover esse usuário');
+        };
 
-    async remove(id: string) {
-        console.log(id);        
+        return await this.userRepo.remove(user);
     }
 }
