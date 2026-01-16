17cbf6ffc2d3ab1a7b10387045b40a9238758c37
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-16 20:42:23 -0300

    documenta UsersController e CreateUserDto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,17 +1,35 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty({
+        example: 'Natan',
+        description: 'nome do usuário',
+        minLength: 3,
+        maxLength: 155,
+    })
     @IsString()
     @MinLength(3)
     @MaxLength(155)
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty({
+        example: 'natan@email.com',
+        description: 'email do usuário',
+        maxLength: 255
+    })
     @IsEmail()
     @MaxLength(255)
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({
+        example: 'senha123',
+        description: 'senha do usuário',
+        minLength: 6,
+        maxLength: 255,
+    })
     @IsString()
     @MinLength(6)
     @MaxLength(255)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,13 +5,18 @@ import { UpdateUserDto } from "./dtos/update-user.dto";
 import { tokenPayloadParams } from "src/auth/params/token-payload.params";
 import { TokenPayloadDto } from "src/auth/dto/token-payload.dto";
 import { AuthTokenGuard } from "src/auth/guards/auth-token.guard";
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor (
         private readonly userService: UsersService,
     ) {}
 
+    @ApiOperation({summary: 'Cria um novo usuário'})
+    @ApiResponse({status: 201, description: 'usuário criado com sucesso'})
+    @ApiResponse({status: 409, description: 'Dados inválidos'})
     @HttpCode(HttpStatus.CREATED)
     @Post()
     async create(@Body() createUserDto: CreateUserDto) {
@@ -19,6 +24,11 @@ export class UsersController {
     }
 
     @UseGuards(AuthTokenGuard)
+    @ApiOperation({summary: 'Retorna um usuário específico pelo ID'})
+    @ApiParam({name: 'id', description: 'Id do usuário'})
+    @ApiResponse({status: 200, description: 'usuário retornado com sucesso'})
+    @ApiResponse({status: 404, description: 'usuário não encontrado'})
+    @ApiBearerAuth()
     @HttpCode(HttpStatus.OK)
     @Get(':id')
     async findOne(@Param('id') id: string) {
@@ -26,6 +36,12 @@ export class UsersController {
     }
 
     @UseGuards(AuthTokenGuard)
+    @ApiOperation({summary: 'Atualiza um usuário existente'})
+    @ApiParam({name: 'id', description: 'Id do usuário'})
+    @ApiResponse({status:200, description: 'atualização de usuário bem-sucedida'})
+    @ApiResponse({status:404, description: 'usuário não encontrado'})
+    @ApiResponse({status:403, description: 'não possui permissão para atualizar o usuário'})
+    @ApiBearerAuth()
     @HttpCode(HttpStatus.OK)
     @Patch(':id')
     async update(
@@ -36,6 +52,12 @@ export class UsersController {
     }
 
     @UseGuards(AuthTokenGuard)
+    @ApiOperation({summary: 'Remove um usuário existente'})
+    @ApiParam({name: 'id', description: 'Id do usuário'})
+    @ApiResponse({status: 200, description: 'usuário removido com sucesso'})
+    @ApiResponse({status:404, description: 'usuário não encontrado'})
+    @ApiResponse({status:403, description: 'não possui permissão para remover o usuário'})
+    @ApiBearerAuth()
     @HttpCode(HttpStatus.OK)
     @Delete(':id')
     async remove(@Param('id') id: string, @tokenPayloadParams() tokenPayloadDto:TokenPayloadDto) {
