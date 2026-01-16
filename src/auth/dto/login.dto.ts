6f15b0117420aec5f15f9e04c1ad1aded49da967
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-16 20:40:43 -0300

    documenta authController e loginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginDto {
+    @ApiProperty({
+        example: 'natan@email.com',
+        description: 'Email do usuário',
+        maxLength: 255
+    })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({
+        example: '2206j147',
+        description: 'Senha do usuário',
+        minLength: 6,
+        maxLength: 255
+    })
     @IsString()
     @IsNotEmpty()
     password: string;
