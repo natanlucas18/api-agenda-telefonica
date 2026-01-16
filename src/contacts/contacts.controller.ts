bb72cabbed11f0570c3804692f2b525189b42e25
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-16 20:42:03 -0300

    documenta ContactsController e CreateContactDto

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -4,18 +4,26 @@ import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { PaginationQueryDto } from 'src/common/dtos/pagination-query.dto';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @UseGuards(AuthTokenGuard)
+@ApiBearerAuth()
+@ApiTags('contacts')
 @Controller('contacts')
 export class ContactsController {
-  constructor(private readonly contactsService: ContactsService) {}
+  constructor(private readonly contactsService: ContactsService) { }
 
+  @ApiOperation({ summary: 'Cria um contato' })
+  @ApiResponse({ status: 201, description: 'contato criado com sucesso' })
+  @ApiResponse({ status: 409, description: 'dados inválidos' })
   @HttpCode(HttpStatus.CREATED)
   @Post()
   create(@Body() createContactDto: CreateContactDto) {
     return this.contactsService.create(createContactDto);
   }
 
+  @ApiOperation({ summary: 'Retorna todos os contatos' })
+  @ApiResponse({ status: 200, description: 'contatos retornados com sucesso' })
   @HttpCode(HttpStatus.OK)
   @Get()
   findAll(
@@ -24,18 +32,31 @@ export class ContactsController {
     return this.contactsService.findAll(query);
   }
 
+  @ApiOperation({ summary: 'Retorna um contato específico pelo ID' })
+  @ApiParam({ name: 'id', description: 'Id do contato' })
+  @ApiResponse({ status: 200, description: 'contato retornado com sucesso' })
+  @ApiResponse({ status: 404, description: 'contato não encontrado' })
   @HttpCode(HttpStatus.OK)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.contactsService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Atualiza um contato existente' })
+  @ApiParam({ name: 'id', description: 'Id do contato' })
+  @ApiResponse({ status: 200, description: 'atualização de contato bem-sucedida' })
+  @ApiResponse({ status: 404, description: 'contato não encontrado' })
+  @ApiResponse({ status: 409, description: 'dados inválidos' })
   @HttpCode(HttpStatus.OK)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
     return this.contactsService.update(id, updateContactDto);
   }
 
+  @ApiOperation({ summary: 'Remove um contato existente' })
+  @ApiParam({ name: 'id', description: 'Id do contato' })
+  @ApiResponse({ status: 200, description: 'contato removido com sucesso'})
+  @ApiResponse({ status: 404, description: 'contato não encontrado' })
   @HttpCode(HttpStatus.OK)
   @Delete(':id')
   remove(@Param('id') id: string) {
--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,16 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateContactDto {
+    @ApiProperty({
+        example: 'João',
+        description: 'nome do contato',
+        minLength: 3,
+        maxLength: 255,
+    })
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(255)
     name: string;
 
+    @ApiProperty({
+        example: 'joao@email.com',
+        description: 'email do contato'
+    })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({
+        example: '87 99566042',
+        description: 'numero de telefone do contato',
+        minLength: 10
+    })
     @IsString()
     @IsNotEmpty()
     @MinLength(10)
