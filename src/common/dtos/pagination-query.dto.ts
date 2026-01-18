d123ae7180eabcf54b49d09f1d200a41debeb22f
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-17 21:08:55 -0300

    adiciona class-transformer para transformar os dados da query

--- src/common/dtos/pagination-query.dto.ts
+++ src/common/dtos/pagination-query.dto.ts
@@ -1,34 +1,59 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsIn } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsOptional,
+  IsInt,
+  Min,
+  IsString,
+  IsIn,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class PaginationQueryDto {
-    @ApiProperty({
-        example: 1,
-        description: 'número da página'
-    })
-    page: number = 1;
+  @ApiProperty({
+    example: 1,
+    description: 'número da página',
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
 
-    @ApiProperty({
-        example: 10,
-        description: 'quantidade de itens'
-    })
-    limit: number = 10;
+  @ApiProperty({
+    example: 10,
+    description: 'quantidade de itens',
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number = 10;
 
-    @ApiProperty({
-        example: 'natan',
-        description: 'filtro de busca'
-    })
-    search?: string;
+  @ApiProperty({
+    example: 'natan',
+    description: 'filtro de busca',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  search?: string;
 
-    @ApiProperty({
-        example: 'id',
-        description: 'ordenar por um campo específico'
-    })
-    sortBy?: string;
+  @ApiProperty({
+    example: 'id',
+    description: 'ordenar por um campo específico',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  sortBy?: string;
 
-    @ApiProperty({
-        example: 'ASC',
-        description: 'ordernação do maior para o menor ou vice-versa'
-    })
-    sortOrder?: 'ASC' | 'DESC';
+  @ApiProperty({
+    example: 'ASC',
+    description: 'ordenação',
+    enum: ['ASC', 'DESC'],
+    required: false,
+  })
+  @IsOptional()
+  @IsIn(['ASC', 'DESC'])
+  sortOrder?: 'ASC' | 'DESC';
 }
